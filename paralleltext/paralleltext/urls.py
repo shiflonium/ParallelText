dfd21e85a4aa07a4e5b24e1f69b794b97f02c136
Kevin Chan <chessmasterhong@yahoo.com>
2013-03-16 23:50:17 -0400

    Implemented basic user registration. Linked home/index, login, register pages together.

--- paralleltext/paralleltext/urls.py
+++ paralleltext/paralleltext/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^paralleltext/', include('paralleltext.foo.urls')),
     url(r'^$', 'paralleltext.views.index'),
     url(r'^login/', 'login.views.user_auth'),
+    url(r'^register/', 'register.views.register'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
