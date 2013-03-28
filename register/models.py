c1de95d9b2c7e09ca3f98d745a00fa5a71c92ee4
Kevin Chan <chessmasterhong@yahoo.com>
2013-03-27 23:28:08 -0400

    Added label and help text to registration form. Commented native language input.

--- register/models.py
+++ register/models.py
@@ -4,17 +4,32 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=False)
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    native_language = forms.ChoiceField(choices=(
-        # Language Codes
-        # http://msdn.microsoft.com/en-us/library/ms533052%28v=vs.85%29.aspx
-        # http://www.lingoes.net/en/translator/langcode.htm
-        ('en',  'English'),
-        ('he',  'Hebrew'),
-        ('es',  'Spanish'),
-    ))
+    email = forms.EmailField(
+        label = 'Email Address',
+        required = False,
+        help_text = "Optional. Enter a valid email address.",
+    )
+    first_name = forms.CharField(
+        #label = 'First Name',
+        required = False,
+        help_text = "Optional. Enter your first name.",
+    )
+    last_name = forms.CharField(
+        #label = 'First Name',
+        required = False,
+        help_text = "Optional. Enter your last name.",
+    )
+    #native_lang = forms.ChoiceField(
+        #label = 'Native Language',
+        #choices = (
+            # Language Codes
+            # http://msdn.microsoft.com/en-us/library/ms533052%28v=vs.85%29.aspx
+            # http://www.lingoes.net/en/translator/langcode.htm
+            #('en',  'English'),
+            #('he',  'Hebrew'),
+            #('es',  'Spanish'),
+        #)
+    #)
 
     class Meta:
         model = User
@@ -25,7 +40,7 @@ class UserCreationForm(UserCreationForm):
             'email',
             'first_name',
             'last_name',
-            'native_language'
+            #'native_lang'
         )
 
     def save(self, commit=True):
@@ -33,6 +48,7 @@ class UserCreationForm(UserCreationForm):
         user.email = self.cleaned_data['email']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        #user.native_lang = native_lang
 
         if commit:
             user.save()
