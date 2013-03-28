a3b98c24a7e7ea4bf67d0805d3224780389bd004
Kevin Chan <chessmasterhong@yahoo.com>
2013-03-27 21:05:59 -0400

    Reverted to Django's HTML registration form. Extended Django's UserCreationForm to include user email input.

--- register/models.py
+++ register/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+#from django.db import models
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your models here.
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "email")
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+
+        return user
+
