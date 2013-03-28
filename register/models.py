bc67835ec7846f58d23cadef8b08dd92186ca833
Kevin Chan <chessmasterhong@yahoo.com>
2013-03-27 22:00:28 -0400

    Extended UserCreationForm with native language drop down menu.

--- register/models.py
+++ register/models.py
@@ -7,13 +7,29 @@ class UserCreationForm(UserCreationForm):
     email = forms.EmailField(required=False)
     first_name = forms.CharField()
     last_name = forms.CharField()
+    native_language = forms.ChoiceField(choices=(
+        # Language Codes
+        # http://msdn.microsoft.com/en-us/library/ms533052%28v=vs.85%29.aspx
+        # http://www.lingoes.net/en/translator/langcode.htm
+        ('en',  'English'),
+        ('he',  'Hebrew'),
+        ('es',  'Spanish'),
+    ))
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', 'email', 'first_name', 'last_name')
+        fields = (
+            'username',
+            'password1',
+            'password2',
+            'email',
+            'first_name',
+            'last_name',
+            'native_language'
+        )
 
     def save(self, commit=True):
-        user = super(UserCreateForm, self).save(commit=False)
+        user = super(UserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
