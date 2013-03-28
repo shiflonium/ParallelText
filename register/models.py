155615ea5753d2039235fcb674c7b90dc5db4326
Kevin Chan <chessmasterhong@yahoo.com>
2013-03-27 21:22:34 -0400

    Extended UserCreationForm with first and last name.

--- register/models.py
+++ register/models.py
@@ -5,14 +5,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreationForm(UserCreationForm):
     email = forms.EmailField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ('username', 'password1', 'password2', 'email', 'first_name', 'last_name')
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
 
         if commit:
             user.save()
