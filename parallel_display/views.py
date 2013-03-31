fbc3b44730b3c5af51290f3ffaa600d746a071e9
Jonathan Shimrony <jshimro00@citymail.cuny.edu>
2013-03-30 21:41:35 -0400

    working parallel display with scroll bar and html files path located on project and not on my computer. This should work

--- parallel_display/views.py
+++ parallel_display/views.py
@@ -2,23 +2,24 @@
 # views.py
 import re
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 page=""
+def getPage(page):
+	soup=BeautifulSoup(open(page))
+	return soup
+
 def parseHtml(filepath):
-	"""parseHtml takes an html/text file path and strips
+	"""parseHtml takes an html file path and strips
 	all tags from it besides <p> tags. the <p> tags are added
 	to a list. parseHtml returns a joined list as a string"""
-	f=open(filepath,"r")
-	initialList=f.readlines()
-	listAsString="".join(initialList)
-	pTagList=re.findall('<p.*?</p>',listAsString,re.DOTALL)
+	page=str(getPage(filepath))
+	pTagList=re.findall('<p.*?</p>',page,re.DOTALL)
 	pTagString="".join(pTagList)
-	f.close()
 	return pTagString
 
 def pdisplay(request):
-	patheng="/home/2bz2/Desktop/pdisplay/paralleltext/parallel_display/ch_1_eng.txt"
-	pathheb="/home/2bz2/Desktop/pdisplay/paralleltext/parallel_display/ch_1_heb.txt"
+	patheng="texts/Bible_Genesis/EN/ch_1.html"
+	pathheb="texts/Bible_Genesis/HE/ch_1.html"
 	eng_cell=parseHtml(patheng)
 	heb_cell=parseHtml(pathheb)
-	print eng_cell
 	return render(request,"parallel_display/display.html",{'eng_text':eng_cell,'heb_text':heb_cell})
