c78b15347af68386cb5f62fbab59151a32272a0a
Mitchell Verter <mitchell.verter@gmail.com>
2013-04-09 06:38:59 -0400

    adding  lint script

---
+++ documentation/lint/lint.sh
@@ -0,0 +1,5 @@
+pylint ../../manage.py ../../home/*py ../../pt_main/*py > ADMIN_LINT
+pylint ../../ptext/*py > LIRON_LINT
+pylint ../../parallel_display/*py > YONATAN_LINT
+pylint ../../register/*.py ../../login/*py > KEVIN_LINT
+
