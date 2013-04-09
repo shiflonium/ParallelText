a4fe6b942932ac4556bbf4d50e40e7e95e5f0894
Liron Shimrony <lsxliron@gmail.com>
2013-04-08 22:18:22 -0400

    merged ptext with parallel_display. ptext library is disabled but CANNOT be erased.

--- ptext/views.py
+++ ptext/views.py
@@ -1,86 +1,71 @@
 #from django.http import HttpResponse
-"""
-This module is used to demonstrate the popup for dictionaries
-The entire module has to be eliminated and integrated into
-the parallel_display module
-"""
+
 from django.shortcuts import render
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+import string
 import re
 
-def get_page(html_page):
-    """
-    This function gets the beautiful soup from a page
-    It probably isn't necessary.
-    Just call beautiful soup
-    """
-    soup = BeautifulSoup(open(html_page))
-    return soup
+#def getPage(htmlPage):
+#	soup = BeautifulSoup(open(htmlPage));
+#	return soup
 
-def remove_commas(my_list, val):
-    """
-    This removes commas from a string
-    Probably isn't necessary as a separate function
-    """
-    return [value for value in my_list if value != val]
+def removeCommas(myList, val):
+	return [value for value in myList if value!=val]
 
 
-def popup_demo(request):
-    """
-    This function iterates through every word in the page
-    and creates a popup text for it
-    
-    This function needs to be integrated into the main 
-    paralleldisplay function
-    
-    Also, you need to fill the popups with dicitonary lookups
-    
-    Look at issue # 64 on how to do this
-    """
-    page = get_page('texts/Bible_Genesis/EN/ch_1.html')    #Get html source
+def stripPage(page):
+	#page = getPage('texts/Bible_Genesis/EN/ch_1.html')	#Get html source
+	#page = getPage(html)	#Get html source
 
-    #STRIP TEXT
-    plist =  re.findall(r'<p.*?</p>', str(page.html), re.DOTALL)
-    stripped_par_list = re.findall(r'>.*?</p>', str(plist), re.DOTALL)
-    stripped_par_list = re.sub(r'\\n', ' ', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\\r', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'</p>', '\|', str(stripped_par_list))
-    stripped_par_list = re.sub(r'>', ' <p> ', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\|', ' </p> ', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\\', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\d\.', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\d', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r',', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'"', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\'', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\[', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\]', '', str(stripped_par_list))
-    stripped_par_list = re.sub(r'\'', '', str(stripped_par_list))
-    
+	#STRIP TEXT
+	plist = re.findall(r'<p.*?</p>',str(page),re.DOTALL)
+	strippedParList = re.findall(r'>.*?</p>',str(plist),re.DOTALL)
+	strippedParList = re.sub(r'\\\\n',' ',str(strippedParList))
+	strippedParList = re.sub(r'\\\\r','',str(strippedParList))
+	strippedParList = re.sub(r'</p>', '|',str(strippedParList))
+	strippedParList = re.sub(r'>', ' <p> ',str(strippedParList))
+	strippedParList = re.sub("\|", ' </p> ',str(strippedParList))
+	strippedParList = re.sub(r'\[', '',str(strippedParList))
+	strippedParList = re.sub(r'\]', '',str(strippedParList))
+	
+	#While loop to create a clean list
+	pos=0
+	text=list()
+	text.append('')
+	i=0
+	
+	while (i<len(strippedParList)):
+		if (strippedParList[i]!=' '):
+			text[pos] = text[pos]+strippedParList[i]
+		
+		else:
+			pos = pos+1
+			text.append('')
+		i=i+1
 
-    #While loop to create a clean list
-    pos = 0
-    test = list()
-    test.append('')
-    i = 0
-    
-    while (i<len(stripped_par_list)):
-        if (stripped_par_list[i]!=' '):
-            test[pos] = test[pos]+stripped_par_list[i]
-        
-        else:
-            pos = pos+1
-            test.append('')
-        i = i+1
-
-        test = remove_commas(test,',')
-        test = remove_commas(test,'\'')
+	#Clean text
+	text = removeCommas(text,'\'')
+	text = removeCommas(text,'')
+	text = removeCommas(text,'\',')
+	text = removeCommas(text,'"')
+	text = removeCommas(text,'",')
 
 
-    
-    return render(request, 'ptext/popupDemo.html',
-                {'myTitle':'Popup Demo', 'css_url':'popup.css',
-                'text':test, 'img_url':'Info.png'}) 
+	#Enocde text to html
+	for i in range (0,len(text)):
+		text[i]=text[i].decode('string_escape')
+		text[i]=text[i].decode('string_escape')
+		
+
+	
+	return text
+
+#def popupDemo(request):
+#	page1 = stripPage('texts/Bible_Genesis/EN/ch_1.html')
+#	page2 = stripPage('texts/Bible_Genesis/HE/ch_1.html')
+	
+#	return render(request, 'ptext/popupDemo.html',{'myTitle':'Demo', 'css_url':'popup.css',
+#		'text1':page1, 'text2':page2, 'img_url':'Info.png'}) 
 
 
 
