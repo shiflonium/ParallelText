07505b37dff6f310faf1bb66da64a3e0a083fa12
Kevin Chan <chessmasterhong@yahoo.com>
2013-04-14 15:57:52 -0400

    Added tests for home page.

--- home/tests.py
+++ home/tests.py
@@ -5,15 +5,33 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+#from django.test import TestCase
+from django.test.client import RequestFactory
+import unittest
+from home.views import index
+#from mock import patch
 
+class TestIndex(unittest.TestCase):
+    """
+    This class is used to test the register module
+    """
+    def setUp(self):
+        """
+        Every test needs access to the request factory.
 
-class SimpleTest(TestCase):
-    """
-    This class is a placeholder for real functionality 
-    """
-    def test_basic_addition(self):
+        Pylint error: Invalid name "setUp" for type method
+                      (should match [a-z_][a-z0-9_]{2,30}$)
+        Comment: Do not change "setUp"; it will break testing otherwise
+                 with test error: ...object has no attribute 'factory'
         """
-        Tests that 1 + 1 always equals 2.
+        self.factory = RequestFactory()
+
+    def test_home_page(self):
         """
-        self.assertEqual(1 + 1, 2)
+        This simulates viewing the home page and verifies
+        that it receives a successful url request
+        """
+        request = self.factory.get('/')
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+
