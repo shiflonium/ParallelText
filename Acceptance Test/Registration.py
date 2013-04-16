a8a01ed945b1e32717c90c0b5e1d207db158e483
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-16 18:08:24 -0400

    adding registration test

---
+++ Acceptance Test/Registration.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
+
+class Test1(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://lit-earth-8332.herokuapp.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_1(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("Register").click()
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("webdesign")
+        driver.find_element_by_id("id_password1").clear()
+        driver.find_element_by_id("id_password1").send_keys("exdx")
+        driver.find_element_by_id("id_password2").clear()
+        driver.find_element_by_id("id_password2").send_keys("exdx")
+        Select(driver.find_element_by_id("id_native_lang")).select_by_visible_text("Hebrew")
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+        driver.find_element_by_link_text("Login").click()
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys("webdesign")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("exdx")
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert.text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
