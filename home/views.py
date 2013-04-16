e41af650e534422e184a1ce1ef49faf418c3161d
Kevin Chan <chessmasterhong@yahoo.com>
2013-04-16 14:16:03 -0400

    Fixed bug where username would not persist in system navigation menu when changing pages.

--- home/views.py
+++ home/views.py
@@ -2,13 +2,17 @@
 This renders the homepage
 """
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def index(request):
-    """ 
+    """
     this method is responsible for drawing the homepage
     """
-    return render(request, 'home/index.html', 
-                  {'include_css': 'home.css', 'include_js':
-                    'slideshow.js', 'onload': 
-                    'playImage(\'slideImg\')'})
+    username = User.objects.get(username=request.user)
+    return render(request, 'home/index.html',
+                  {'include_css': 'home.css',
+                   'include_js': 'slideshow.js',
+                   'onload': 'playImage(\'slideImg\')',
+                   'username': username
+                  })
 
