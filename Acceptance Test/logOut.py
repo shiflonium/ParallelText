c92d01b15f4202e80d281a8cb75d9db85daa1409
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-17 00:54:18 -0400

    logout function

--- Acceptance Test/logOut.py
+++ Acceptance Test/logOut.py
@@ -11,17 +11,15 @@ import unittest, time, re
 
 class Logout(unittest.TestCase):
         def setUp(self):
-            
+            self.driver = webdriver.Firefox()
+            self.driver.implicitly_wait(30)
+            self.base_url = "http://lit-earth-8332.herokuapp.com/"
+            self.verificationErrors = []
+            self.accept_next_alert = True
     
         def test_logout(self):
-           
+            
         
-        
-
-
-
-
-
 
 if __name__ == "__main__":
     unittest.main()
