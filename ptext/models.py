1127a61f89e64cd0e1a1212ae18fd33ca36a9006
Liron Shimrony <lsxliron@gmail.com>
2013-04-18 15:15:38 -0400

    added auto increment to languages tables (as definitionID)

--- ptext/models.py
+++ ptext/models.py
@@ -8,21 +8,31 @@ from django.db import models
 
 class Languages(models.Model):
     '''Dictionary object'''
+    definitionID = models.IntegerField()
     fromLang = models.CharField(max_length=2)
     toLang = models.CharField(max_length=2)
     original = models.CharField(primary_key=True, max_length = 20)
     definition = models.CharField(max_length = 200)
     
+    def save(self):
+		top = Languages.objects.order_by('definitionID')
 
-'''
+		if len(top) == 0:
+			self.definitionID=0
+			super(Languages, self).save()
+		else:
+			top = top[len(top)-1]
+			self.definitionID = top.definitionID + 1
+			super(Languages,self).save()
+
+
 class UserDictionary(models.Model):
-	fromLang = models.CharField(max_length=2)
-	toLang = models.CharField(max_length=2)
+	userID = models.CharField(max_length=2)
+	definitionID = models.CharField(max_length=2)
 
-class Languages(models.Model):
+class AvailLangs(models.Model):
 	fromLang = models.CharField(max_length=2)
 	toLang = models.CharField(max_length=2)
 
 	class Meta:
 		unique_together = ('fromLang', 'toLang')
-'''
