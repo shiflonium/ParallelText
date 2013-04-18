ec597b57de5d4dfeeb13469498b555de7f5d7f81
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-18 17:38:47 -0400

    improving the score

--- Acceptance Test/MissingRegInfo.py
+++ Acceptance Test/MissingRegInfo.py
@@ -9,7 +9,13 @@ from selenium.common.exceptions import NoSuchElementException
 import unittest
 
 class FailReg(unittest.TestCase):
+    '''
+    
+    '''
     def setUp(self):
+        '''
+        
+        '''
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "http://lit-earth-8332.herokuapp.com/"
@@ -17,6 +23,9 @@ class FailReg(unittest.TestCase):
         self.accept_next_alert = True
     
     def test_fail_reg(self):
+        '''
+        d
+        '''
         driver = self.driver
         driver.get(self.base_url + "/")
         driver.find_element_by_link_text("Register").click()
@@ -25,8 +34,13 @@ class FailReg(unittest.TestCase):
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        '''
+        
+        '''
+        try: 
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException:
+            return False
         return True
     
     def close_alert_and_get_its_text(self):
