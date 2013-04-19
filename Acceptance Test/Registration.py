140eae252aaf78430fd3a0be89c2dd41e532f93d
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-18 21:23:40 -0400

    done with pylint fix

--- Acceptance Test/Registration.py
+++ Acceptance Test/Registration.py
@@ -4,12 +4,14 @@ register with the site and logs into the system.
 '''
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
+import unittest
 
 class Test1(unittest.TestCase):
+    '''
+    defining the test1 class
+    '''
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -18,6 +20,9 @@ class Test1(unittest.TestCase):
         self.accept_next_alert = True
     
     def test_1(self):
+        '''
+        defining the function
+        '''
         driver = self.driver
         driver.get(self.base_url + "/")
         driver.find_element_by_link_text("Register").click()
@@ -37,11 +42,19 @@ class Test1(unittest.TestCase):
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        '''
+        defining the function
+        '''
+        try: 
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException: 
+            return False
         return True
     
     def close_alert_and_get_its_text(self):
+        '''
+        defining the function
+        '''
         try:
             alert = self.driver.switch_to_alert()
             if self.accept_next_alert:
