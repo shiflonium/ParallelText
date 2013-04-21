57a309973fdf5c9a5560857ae5c1b61e1f457241
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-21 02:31:53 -0400

    having doc string

--- search/models.py
+++ search/models.py
@@ -5,4 +5,8 @@ from django.db import models
 from django.shortcuts import render
 
 def search(request):
+    '''
+    this is the search function which
+    shows up the search page
+    '''
     return render(request, 'search/search.html')
