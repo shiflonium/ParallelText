27920a2f5d6a23710fda50ec782d765a97fe9fe8
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-21 02:11:13 -0400

    url got updated

--- search/views.py
+++ search/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+
+from django.shortcuts import render
+#from django.contrib.auth.models import User
+
+def search(request):
+    """
+    this method is responsible for drawing the search page
+    """
+    #username = User.objects.get(username=request.user)
+    return render(request, 'search/search.html',
+                  {'include_css': 'home.css',
+                   'include_js': 'slideshow.js',
+                   'onload': 'playImage(\'slideImg\')',
+                   #'username': username
+                  })
