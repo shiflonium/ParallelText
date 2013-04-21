2b641a556ba110a4553119b6cb397fdfbd008d97
Emdad Ahmed <emdadnrn@gmail.com>
2013-04-21 01:52:08 -0400

    change to sqlite3 just for my development, will restore to postgresql_psycopg2 before I push to my bitbucket

--- pt_main/settings.py
+++ pt_main/settings.py
@@ -17,13 +17,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # django.db.backends.postgresql_psycopg2'
         'NAME': os.path.join(PROJECT_ROOT, 'book.db'), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'paralleltext',
-        'PASSWORD': 'knight',
-        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
+        #'USER': 'paralleltext',
+        #'PASSWORD': 'knight',
+        #'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '',  # Set to empty string for default.
     }
 }
 
