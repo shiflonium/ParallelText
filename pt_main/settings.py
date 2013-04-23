76f59dcfbe86db072b5dbba8879c8c828d0b84ff
Mitchell Verter <mitchell.verter@gmail.com>
2013-04-23 17:08:51 -0400

    hi

--- pt_main/settings.py
+++ pt_main/settings.py
@@ -17,11 +17,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(PROJECT_ROOT, 'book.db'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # django.db.backends.postgresql_psycopg2'
+        'NAME':  'book.db', # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
         'USER': 'paralleltext',
         'PASSWORD': 'knight',
-        'HOST': 'localhost',
+        'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
 }
@@ -173,9 +175,3 @@ LOGGING = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
