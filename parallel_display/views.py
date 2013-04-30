485f9a5abf08ed57f77b4cf6b9d8f40175abd8dd
Jonathan Shimrony <jshimro00@citymail.cuny.edu>
2013-04-30 17:38:15 -0400

    imporoved pylint score

--- parallel_display/views.py
+++ parallel_display/views.py
@@ -18,56 +18,64 @@ localhost/parallel_display/Bible_Genesis/ch_1/ENHE/
 import re
 from books.models import BookInfo
 from django.shortcuts import render
-from django.core.context_processors import csrf
 from bs4 import BeautifulSoup
 from ptext.views import strip_page
 from django import forms
-from django.utils.safestring import mark_safe
+
 page = ""
 
 
 left_lang = (
-        ("en","English"),
-        ("he","Hebrew"),
-        ("el","Greek"),
-        ("es","Spanish"),
-        ("pt","Portuguese"),
-        ("ru","Russian"),
-        ("ar","Arabic"),
+        ("en", "English"),
+        ("he", "Hebrew"),
+        ("el", "Greek"),
+        ("es", "Spanish"),
+        ("pt", "Portuguese"),
+        ("ru", "Russian"),
+        ("ar", "Arabic"),
         )
 
 right_lang = (
-        ("en","English"),
-        ("he","Hebrew"),
-        ("el","Greek"),
-        ("es","Spanish"),
-        ("pt","Portuguese"),
-        ("ru","Russian"),
-        ("ar","Arabic"),
+        ("en", "English"),
+        ("he", "Hebrew"),
+        ("el", "Greek"),
+        ("es", "Spanish"),
+        ("pt", "Portuguese"),
+        ("ru", "Russian"),
+        ("ar", "Arabic"),
         )
 
 
 
 class Texts(forms.Form):
-    b=BookInfo.objects.filter(title="Quran")
-    chap_num_str=b[0].chaps
-    chap_num=int(chap_num_str)
-    chap_choices=[]
-    for i in range (1,chap_num):
-        chap_choices.append(("ch_"+str(i),"Chapter "+str(i)))
-    choices_final=tuple(chap_choices)
-    book_dd = forms.ModelChoiceField(empty_label="Select Book",label = "Book",queryset=(BookInfo.objects.all().values_list('title',flat=True)))
-    chapter_dd = forms.ChoiceField(label = "Chapter",required=False,choices=choices_final)
-    right_lang_dd = forms.ChoiceField(label = "Right Language",choices=right_lang)
-    left_lang_dd = forms.ChoiceField(label = "Left Language",choices = left_lang)
+    """
+    This class is the form that has dropdown select boxes
+    it uses modelchoicefield and choicefield, part of django forms
+
+    in the future we would like to 
+    create dynamic select boxes.
+    """
+    b = BookInfo.objects.filter(title="Quran")
+    chap_num_str = b[0].chaps
+    chap_num = int(chap_num_str)
+    chap_choices = []
+    for i in range (1, chap_num):
+        chap_choices.append(("ch_" + str(i), "Chapter " + str(i)))
+    choices_final = tuple(chap_choices)
+    book_dd = forms.ModelChoiceField(
+    empty_label = "Select Book", label = "Book", 
+    queryset = (BookInfo.objects.all().values_list('title', flat=True)))
+    chapter_dd = forms.ChoiceField(label = "Chapter", 
+    required=False, choices=choices_final)
+    right_lang_dd = forms.ChoiceField(label = "Right Language",
+    choices=right_lang)
+    left_lang_dd = forms.ChoiceField(label = "Left Language",
+    choices = left_lang)
     search_field = forms.CharField(label = "Search Texts")
     #class Texts end
 
 
-   
-#visual_dropdown = Texts(auto_id = False)
-#print visual_dropdown
-def get_page(page):
+def get_page(import_page):
     """
     This function grabs the page and turns it into a
     beautiful soup.
@@ -75,15 +83,15 @@ def get_page(page):
     It is unclear to me whether we actually need a separate
     function for this.
     """
-    soup = BeautifulSoup(open(page))
+    soup = BeautifulSoup(open(import_page))
     return soup
 
 def parse_html(filepath):
     """parse_html takes an html file path and strips
     all tags from it besides <p> tags. the <p> tags are added
     to a list. parse_html returns a joined list as a string"""
-    page = str(get_page(filepath))
-    p_tag_list = re.findall('<p.*?</p>' , page , re.DOTALL)
+    html_page = str(get_page(filepath))
+    p_tag_list = re.findall('<p.*?</p>' , html_page , re.DOTALL)
     p_tag_string = "" . join(p_tag_list)
     return p_tag_string
 
@@ -113,15 +121,12 @@ def pdisplay(request):
 
 
     if request.method == "POST":
-        #posted_form = Texts(request.POST)
         book = request.POST["book_dd"]
-        print book
         chapter = request.POST['chapter_dd']
         from_lang = request.POST['left_lang_dd']
         to_lang = request.POST['right_lang_dd']
         path1 = "texts/"+book+"/"+from_lang.upper()+"/"+chapter+".html"
         path2 = "texts/"+book+"/"+to_lang.upper()+"/"+chapter+".html"
-        print path1
     else:
         path1 = "texts/Bible_Genesis/EN/ch_1.html"
         path2 = "texts/Bible_Genesis/HE/ch_1.html"
