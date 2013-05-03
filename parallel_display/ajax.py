ca073125d57f18e973385a85a70d903c8b7f1229
Liron Shimrony <lsxliron@gmail.com>
2013-05-03 18:10:28 -0400

    fixed pylint

--- parallel_display/ajax.py
+++ parallel_display/ajax.py
@@ -1,33 +1,39 @@
-from dajax.core import Dajax
+'''DAJAX'''
 from dajaxice.decorators import dajaxice_register
 from django.utils import simplejson
 from django.contrib.auth.models import User
 from languages.models import Translations
 from languages.models import UserDictionary
 
-@dajaxice_register
-def insertWord(request, word):
-	print "HI"
 
-	#Check if the user is connected
-	auth = request.user.is_authenticated()
-	print "AUTH:" + str(auth)
-	word = word.replace(' ','')
-	print "ORIGINAL WORD: '" + word +"'"
+
+@dajaxice_register
+def insert_word(request, word):
+    '''
+    This function inserting a word into the UserDictionary.
+    The user has to be logged in in order to use this feature
+    '''    
+
+    #Check if the user is connected
+    auth = request.user.is_authenticated()
+	#print "AUTH:" + str(auth)
+    word = word.replace(' ', '')
+	#print "ORIGINAL WORD: '" + word +"'"
 
 
 	#USERNAME
-	if(auth):
-		username = request.user
-		currentUser = User.objects.get(username=username)
-		uniword = unicode(word)
-		print "UNIWORD:" + uniword
-		currentWord = Translations.objects.get(original = uniword)
-		print "USERNAME:" + str(username)
-		print "currentUser:" + str(currentUser)
-		print 'currentWord:' + str(currentWord)
-		savedDefinition = UserDictionary(userID=currentUser.id, definitionID=currentWord.definitionID)
-		savedDefinition.save();
-		return simplejson.dumps({'result':auth, 'word':word})
-	else:
-		return simplejson.dumps({'result':auth})
+    if(auth):
+        username = request.user
+        current_user = User.objects.get(username=username)
+        uniword = unicode(word)
+		#print "UNIWORD:" + uniword
+        current_word = Translations.objects.get(original = uniword)
+		#print "USERNAME:" + str(username)
+		#print "currentUser:" + str(currentUser)
+		#print 'currentWord:' + str(currentWord)
+        saved_definition = UserDictionary(
+        	userID=current_user.id, definitionID=current_word.definitionID)
+        saved_definition.save()
+        return simplejson.dumps({'result':auth, 'word':word})
+    else:
+        return simplejson.dumps({'result':auth})
