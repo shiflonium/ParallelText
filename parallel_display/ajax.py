0c9d91a9cd1382d0b5cbd5ef8ca1ed89dd669500
Liron Shimrony <lsxliron@gmail.com>
2013-05-03 13:04:57 -0400

    added dajax support and dajax filee, also changed settings.py and urls.py

---
+++ parallel_display/ajax.py
@@ -0,0 +1,12 @@
+from dajax.core import Dajax
+from dajaxice.decorators import dajaxice_register
+from django.utils import simplejson
+
+@dajaxice_register
+def multiply(request, a, b):
+    dajax = Dajax()
+    result = int(a) * int(b)
+    dajax.assign('#result','value',str(result))
+    #return dajax.json()
+    print "HELLO"
+    return simplejson.dumps({'result':result})
