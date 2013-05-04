44f58d0d88e37851d1905f1d00fe4d65905832c0
Liron Shimrony <lsxliron@gmail.com>
2013-05-03 22:42:27 -0400

    fixed tests for ptext and parallel_display

--- ptext/test.py
+++ ptext/test.py
@@ -8,7 +8,8 @@ from bs4 import BeautifulSoup
 import re
 
 
-EN = BeautifulSoup(open("../texts/Bible_Genesis/EN/ch_1.html"))
+LEFT = BeautifulSoup(open("../texts/Bible_Genesis/EN/ch_1.html"))
+
 
 class TestPText(unittest.TestCase):
     '''
@@ -23,27 +24,33 @@ class TestPText(unittest.TestCase):
             'http://127.0.0.1:8000/parallel_display/').read())
 
 
-    def test_num_paragraphs(self):
-        '''compare the number of paragrpahs 
-	between the original file and the fetched page'''
+    def test_num_paragraphs_left(self):
+        '''
+        compare the number of paragrpahs 
+        between the original file and the fetched page
+        '''
         #paragraphs in EN
-        parEN = re.findall(r'<p.*?</p>', str(EN), re.DOTALL)
+        #par_en = re.findall(r'<p.*?</p>', str(LEFT), re.DOTALL)
+        par_en = re.findall(r'<p.*?</p>', str(LEFT), re.DOTALL)
 
         #paragraphs in self
-        par_self = re.findall(r'<td.*?</td>', str(self.html), re.DOTALL)
-        par_self = re.findall(r'<p.*?</p>', par_self[0], re.DOTALL)
+        par_self = re.findall(r'<table id=\"left_table.*?</table>'
+            , str(self.html),re.DOTALL)
+        #par_self = re.findall(r'<td.*?</td>', str(self.html), re.DOTALL)
+        par_self = re.findall(r'<p.*?</p>', 
+            str(par_self), re.DOTALL)
         
-        string_report = "Original file has {} paragraphs and self has {} paragraphs".format(
-		len(parEN), len(par_self))
-        self.assertEqual(len(parEN), len(par_self), string_report)
-
+        string_report = '''Original file has {} paragraphs and 
+            self has {} paragraphs'''.format(
+                len(par_en), len(par_self))
+        self.assertEqual(len(par_en), len(par_self), string_report)
 
 
     def test_popups(self):
         ''' compare the number of words 
 	between the original file and the fetched page '''
         #Make a long string from EN
-        en_content = re.findall(r'<p.*?</p>', str(EN), re.DOTALL)
+        en_content = re.findall(r'<p.*?</p>', str(LEFT), re.DOTALL)
         en_content = re.findall(r'>.*?</p>', str(en_content), re.DOTALL)
         en_content = str(en_content).replace('</p>','')
         en_content = en_content.decode('string_escape')
@@ -59,10 +66,14 @@ class TestPText(unittest.TestCase):
         en_content = en_content.split()
 
 
-        #Make a long string from slef
-        self_content = re.findall(r'<td.*?</td>', str(self.html), re.DOTALL)
-        self_content = re.findall(r'<em>.*?\b[^\W]+\b.*?<br>', 
-				  self_content[0], re.DOTALL)
+        #Make a long string from self
+        self_content = re.findall(r'<table id=\"left_table\".*?</table>', 
+            str(self.html), re.DOTALL)
+        #self_content = re.findall(r'<td.*?</td>', str(self.html), re.DOTALL)
+        self_content = re.findall(r'data-content.*?data-original', 
+				  str(self_content), re.DOTALL)
+        self_content = str(self_content).replace('data-original','')
+        self_content = str(self_content).replace('data-content','')
         self_content = str(self_content).replace('\\t','')
         self_content = str(self_content).replace('\\n','')
         self_content = str(self_content).replace('\'','')
@@ -70,10 +81,10 @@ class TestPText(unittest.TestCase):
         self_content = str(self_content).replace(',', '')
         self_content = self_content.split()
 
-        s = "Original file has {} words and self has {} words".format(
-        len(en_content), len(self_content))
+        string_report = '''Original file has {} words and self 
+        has {} words'''.format(len(en_content), len(self_content))
         
-        self.assertEqual(len(en_content), len(self_content), s)
+        self.assertEqual(len(en_content), len(self_content)-1, string_report)
 
 
 
