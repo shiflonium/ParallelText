ffb3d5b43b798d2c9e2f569f69a68f6deb13a302
Mitchell Verter <mitchell.verter@gmail.com>
2013-05-06 00:38:56 -0400

    adding english words t dictionary

--- content_mgmt/scripts/grab_English_genesis.sh
+++ content_mgmt/scripts/grab_English_genesis.sh
@@ -2,5 +2,5 @@
 
 for ((i=1; i <=50; i++))
 do
-    python ../../content_mgmt/scripts/dictionaryGrabber.py -o HE -d EN -f=/home/mitchell/ComputerScience/473_WebsiteDesign/DEVELOPERparalleltext/paralleltext/texts/Bible_Genesis/EN/ch_i.html
+    python ../../content_mgmt/scripts/dictionaryGrabber.py -o HE -d EN -f=/home/mitchell/DEVELOPERparalleltextDEVELOPER/paralleltext/texts/Bible_Genesis/EN/ch_$i.html
 done
--- content_mgmt/scripts/grab_hebrew_genesis.sh
+++ content_mgmt/scripts/grab_hebrew_genesis.sh
@@ -1,4 +1,4 @@
 for ((i=1; i <=50; i++))
 do
-    python dictionaryGrabber.py -o HE -d EN -f=/home/mitchell/ComputerScience/473_WebsiteDesign/DEVELOPERparalleltext/paralleltext/texts/Bible_Genesis/HE/ch_$i.html
+    python dictionaryGrabber.py -o HE -d EN -f=/home/mitchell/DEVELOPERparalleltextDEVELOPER/paralleltext/texts/Bible_Genesis/HE/ch_$i.html
 done
