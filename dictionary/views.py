50e0bf346a286fa533bbbb819e367cd3d844d151
Liron Shimrony <lsxliron@gmail.com>
2013-05-07 17:43:46 -0400

    fixed user dictionary display

--- dictionary/views.py
+++ dictionary/views.py
@@ -9,6 +9,7 @@ from languages.models import Languages
 from dictionary.forms import DictLangForm
 from languages.models import UserDictionary
 from ptext.views import CustomDict
+from languages.models import Translations
 
 def viewdict(request):
     """
@@ -21,15 +22,24 @@ def viewdict(request):
         username = User.objects.get(username=request.user).username
         form = DictLangForm(request.GET)
         user_dictionary_list = UserDictionary.objects.filter(userID = userid)
+        
         definitions_list = list()
+        translation_list = list()
+        original_list = list()
+        
+        #Create definition id's list
         for i in range (0,len(user_dictionary_list)):
             definitions_list.append(user_dictionary_list[i].definitionID)
 
+        #Create translation list
+        for i in range (0,len(definitions_list)):
+            original_list.append(Translations.objects.get(definitionID=definitions_list[i]).original)
+            translation_list.append(Translations.objects.get(definitionID=definitions_list[i]).definition)
+
+        words = CustomDict(original_list,translation_list)
 
 
-        print definitions_list
-
-        return render(request, 'dictionary/dictionary.html',
-                      {'form': form,
+        return render(request, 'dictionary/dictionary.html', {'words':words,
+                      'form': form,
                        'username': username})
 
