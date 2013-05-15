3cebd5d51f03ccbce7b978704b02770019ce1654
Mitchell Verter <mitchell.verter@gmail.com>
2013-05-15 19:17:08 -0400

    merging

--- pt_main/settings.py
+++ pt_main/settings.py
@@ -175,9 +175,12 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+
+DEBUG=True
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
