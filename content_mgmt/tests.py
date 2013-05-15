2edfc3c48f0e462dbdc201a1f3972bc87e5cce3d
Mitchell Verter <mitchell.verter@gmail.com>
2013-05-15 13:13:04 -0400

    adding test for content management

--- content_mgmt/tests.py
+++ content_mgmt/tests.py
@@ -4,13 +4,28 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
 from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+import unittest
+from django.test.client import RequestFactory
+from content_mgmt.views import upload
+from content_mgmt.forms import UploadForm
+class TestContentManager(TestCase):
+    def setUp(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        self.factory=RequestFactory()
+
+    def test_upload_response(self):
+        """
+        This simulates a login and verifies
+        that it receives a successful url request
+        """
+        request = self.factory.post('/upload/', POST={
+                'language':"DE",
+                'title':"dummy",
+                'file':"dummyfile.txt"
+                })
+        response = upload(request)
+        self.assertEqual(response.status_code, 200)
+
