c37e46a2950890fd4658ef1d72823edc1c4bac26
Liron Shimrony <lsxliron@gmail.com>
2013-05-14 23:50:59 -0400

    Fixed test for parallel_display and ptext

--- ptext/test.py
+++ ptext/test.py
@@ -1,11 +1,16 @@
+
 '''This class designed to test the popups module. Later on it will be extended
    to test the dictionary functions as well
 '''
 
 import unittest
-import urllib2
+#import urllib2
 from bs4 import BeautifulSoup
 import re
+from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.common.exceptions import NoSuchElementException
 
 
 LEFT = BeautifulSoup(open("../texts/Bible_Genesis/EN/ch_1.html"))
@@ -16,12 +21,16 @@ class TestPText(unittest.TestCase):
     This class designed to test the popups module. Later on it will be extended
     to test the dictionary functions as well
     '''
+
     def setUp(self):
         '''
-	This sets up the test of thepopups
-	'''
-        self.html = BeautifulSoup(urllib2.urlopen (
-            'http://127.0.0.1:8000/parallel_display/').read())
+        This sets up the test of thepopups
+        '''
+        self.driver = webdriver.Firefox()
+        self.base_url = "http://127.0.0.1:8000"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+        self.html = ""
 
 
     def test_num_paragraphs_left(self):
@@ -29,26 +38,84 @@ class TestPText(unittest.TestCase):
         compare the number of paragrpahs 
         between the original file and the fetched page
         '''
-        #paragraphs in EN
-        #par_en = re.findall(r'<p.*?</p>', str(LEFT), re.DOTALL)
+        #Getting the source code
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("Read Books").click()
+        combox = driver.find_element_by_id("id_book_dd")
+        for option in combox.find_elements_by_tag_name("option"):
+            if (option.text == 'Bible Bible Genesis'):
+                option.click()
+        
+
+        driver.find_element_by_name("book_submit").click()
+        combox = driver.find_element_by_id('id_left_lang_dd')
+        for option in combox.find_elements_by_tag_name('option'):
+            if (option.text == 'English'):
+                option.click()
+
+        combox = driver.find_element_by_id('id_right_lang_dd')
+        for option in combox.find_elements_by_tag_name('option'):
+            if (option.text == 'Hebrew'):
+                option.click()
+
+        
+        driver.find_element_by_name("chap_lang_submit").click()
+        
+        self.html = driver.page_source#.encode('utf8')
+        
+
         par_en = re.findall(r'<p.*?</p>', str(LEFT), re.DOTALL)
 
         #paragraphs in self
-        par_self = re.findall(r'<table id=\"left_table.*?</table>'
-            , str(self.html),re.DOTALL)
-        #par_self = re.findall(r'<td.*?</td>', str(self.html), re.DOTALL)
-        par_self = re.findall(r'<p.*?</p>', 
-            str(par_self), re.DOTALL)
+
+        par_self = re.findall(r'<table.*?/table>',
+            self.html.encode('utf-8'),re.DOTALL)
+        
+        par_self = re.findall(r'<p.*?/p>', 
+            par_self[1], re.DOTALL)
         
         string_report = '''Original file has {} paragraphs and 
             self has {} paragraphs'''.format(
                 len(par_en), len(par_self))
+
         self.assertEqual(len(par_en), len(par_self), string_report)
 
 
+
+
     def test_popups(self):
-        ''' compare the number of words 
-	between the original file and the fetched page '''
+        ''' 
+        compare the number of words 
+        between the original file and the fetched page 
+        '''
+
+        #Getting the source code
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("Read Books").click()
+        combox = driver.find_element_by_id("id_book_dd")
+        for option in combox.find_elements_by_tag_name("option"):
+            if (option.text == 'Bible Bible Genesis'):
+                option.click()
+        
+
+        driver.find_element_by_name("book_submit").click()
+        combox = driver.find_element_by_id('id_left_lang_dd')
+        for option in combox.find_elements_by_tag_name('option'):
+            if (option.text == 'English'):
+                option.click()
+
+        combox = driver.find_element_by_id('id_right_lang_dd')
+        for option in combox.find_elements_by_tag_name('option'):
+            if (option.text == 'Hebrew'):
+                option.click()
+
+        
+        driver.find_element_by_name("chap_lang_submit").click()
+        
+        self.html = driver.page_source
+
         #Make a long string from EN
         en_content = re.findall(r'<p.*?</p>', str(LEFT), re.DOTALL)
         en_content = re.findall(r'>.*?</p>', str(en_content), re.DOTALL)
@@ -67,26 +134,22 @@ class TestPText(unittest.TestCase):
 
 
         #Make a long string from self
-        self_content = re.findall(r'<table id=\"left_table\".*?</table>', 
-            str(self.html), re.DOTALL)
-        #self_content = re.findall(r'<td.*?</td>', str(self.html), re.DOTALL)
-        self_content = re.findall(r'data-content.*?data-original', 
-				  str(self_content), re.DOTALL)
-        self_content = str(self_content).replace('data-original','')
-        self_content = str(self_content).replace('data-content','')
-        self_content = str(self_content).replace('\\t','')
-        self_content = str(self_content).replace('\\n','')
-        self_content = str(self_content).replace('\'','')
-        self_content = ''.join(self_content)
-        self_content = str(self_content).replace(',', '')
-        self_content = self_content.split()
+        self_content = re.findall(r'<table.*?</table>', 
+            self.html, re.DOTALL)
+        self_content = re.findall(r'data-original-title=.*?data',
+            self_content[0],re.DOTALL)
 
         string_report = '''Original file has {} words and self 
         has {} words'''.format(len(en_content), len(self_content))
-        
-        self.assertEqual(len(en_content), len(self_content)-1, string_report)
+        self.assertEqual(len(en_content), len(self_content), string_report)
 
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 
 if __name__ ==  '__main__':
     unittest.main()
+
+
